dd0fdd928ebab685391f4d890a1220910fc0472e
MarceloHaa <marcelohenrique.a@hotmail.com>
2024-11-05 17:42:48 -0300

    correção na paginação

--- src/Pages/ExpenseTypeManager/index.js
+++ src/Pages/ExpenseTypeManager/index.js
@@ -91,7 +91,6 @@ const ExpenseTypeManager = () => {
     const handlePageChange = (newPageIndex) => {
         setPagination((prev) => ({ ...prev, pageIndex: newPageIndex }));
         setIsLoading(true);
-        fetchExpenseTypes();
     };
 
     const handleEdit = (id) => {
